346d4a1c28fcd2af02f6bebbce4c0a62cb888cbd
houyabin <hyb459840110@aliyun.com>
2020-08-07 13:54:30 +0800

    整合提交

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,11 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("不知道说点啥");
+		System.out.println("涓嶇煡閬撹鐐瑰暐");
 
-		System.out.println("这是Demo1的提交");
+		System.out.println("杩欐槸Demo1鐨勬彁浜�");
 
-		System.out.println("第二次不知道说点啥");
+		System.out.println("绗簩娆′笉鐭ラ亾璇寸偣鍟�");
 
+		System.out.println("abcdef123456");
 	}
 }
